040bce531dc0551f191d152638c08246b68c114a
Nathan <nathanbaylon@gmail.com>
2025-01-30 17:06:54 +1100

    Refactor code by implementing Application class to handle application logic

--- Application.h
+++ Application.h
@@ -1,8 +1,34 @@
 #pragma once
 
+#include "stb_image.h"
+#include "FastNoiseLite.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <cmath>
+#include <iostream>
+
+#include "Shader.h"
+#include "Camera.h"
+
 namespace Margit {
 	class Application
 	{
+	public:
+		int run();
+
+		~Application();
+
+	private:
+		static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+		static void processInput(GLFWwindow* window);
+
+		static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+		static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 	};
 }
 
